b79d87cecbf58b8bd378290efd8d21b25c0f64c4
runsisi <runsisi@hust.edu.cn>
2023-03-15 22:54:14 +0800

    gbm import
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- gbm/gbm.cc
+++ gbm/gbm.cc
@@ -6,8 +6,10 @@
 #include <unistd.h>
 
 #include <gbm.h>
+#include <xf86drmMode.h>
 
 int main() {
+    int err = 0;
     int fd = open("/dev/dri/card0", O_RDWR | FD_CLOEXEC);
     if (fd < 0) {
         int err = errno;
@@ -21,8 +23,30 @@ int main() {
         exit(1);
     }
 
+    drmModePlaneRes *res = drmModeGetPlaneResources(fd);
+    if (!res) {
+        err = errno;
+        fprintf(stderr, "drmModeGetPlaneResources failed: %s\n", strerror(err));
+        exit(1);
+    }
 
+    for (int i = 0; i < res->count_planes; i++) {
+        drmModePlane *plane = drmModeGetPlane(fd, res->planes[i]);
+        if (!plane) {
+            err = errno;
+            fprintf(stderr, "drmModeGetPlane failed: %s\n", strerror(err));
+            continue;
+        }
+
+        if (plane->fb_id == 0) {
+            drmModeFreePlane(plane);
+            continue;
+        }
+    }
+
+    gbm_bo_import(gbm, GBM_BO_IMPORT_FD, NULL, 0);
 
     gbm_device_destroy(gbm);
+    drmModeFreePlaneResources(res);
     close(fd);
 }
