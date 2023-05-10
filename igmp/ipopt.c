3410a74272ba6a86ddbc170c7ca7711da0820880
runsisi <runsisi@hust.edu.cn>
2023-05-10 09:05:31 +0800

    igmp: add ping length
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- igmp/ipopt.c
+++ igmp/ipopt.c
@@ -16,13 +16,23 @@
 
 #define PROG_NAME					"ipopt"
 
+// IP options
+enum {
+    EOOL = 0,
+    NOP = 1,
+    SID = 136,
+    RTRALT = 148,
+};
+
 #define DEFAULT_INTERVAL            1   // 1s
+#define DEFAULT_DATA_LENGTH         56
 
 static struct {
     char *if_name;                  // network interface for binding
     unsigned int interval;          // send interval
     struct in_addr addr;            // ping address
     uint8_t option;                 // IP option
+    int data_len;              // ping data length
 } args;
 
 atomic_int stopped = 0;
@@ -33,28 +43,25 @@ static void signal_handler(int signal) {
     }
 }
 
-/* Calculate checksum of array of 2-byte words in network order (big-endian).
- * Returns value in host's byte order. */
 uint16_t checksum(uint16_t *addr, int len) {
     uint32_t sum = 0;
 
-    // Sum of each 2-byte values
     while (len > 1) {
-        sum += ntohs(*(addr++));
+        sum += *addr++;
+        if (sum & 0x80000000) {
+            sum = (sum & 0xffff) + (sum >> 16);
+        }
         len -= 2;
     }
 
-    // Add partial block if available at the end of data
-    if (len > 0) {
+    if (len) {
         sum += *(uint8_t *)addr;
     }
 
-    // Sum lower 16 bits and upper 16 bits
     while (sum >> 16) {
         sum = (sum & 0xffff) + (sum >> 16);
     }
 
-    // Return one's compliment of sum.
     return (uint16_t)(~sum);
 }
 
@@ -66,6 +73,7 @@ void print_usage(void) {
     fprintf(stderr, "    -a address     : Specify ping address.\n");
     fprintf(stderr, "    -r interval    : Set ping interval in seconds. Default: %d.\n", DEFAULT_INTERVAL);
     fprintf(stderr, "    -o option      : IP option (0/1/136/148). Default: None.\n");
+    fprintf(stderr, "    -l length      : Ping data length. Default: %d.\n", DEFAULT_DATA_LENGTH);
 }
 
 int main(int argc, char **argv) {
@@ -73,8 +81,9 @@ int main(int argc, char **argv) {
 
     args.interval = DEFAULT_INTERVAL;
     args.option = (uint8_t)-1;
+    args.data_len = DEFAULT_DATA_LENGTH;
 
-    while ((opt = getopt(argc, argv, "hi:r:a:o:")) != -1) {
+    while ((opt = getopt(argc, argv, "hi:r:a:o:l:")) != -1) {
         switch (opt) {
         case 'h':
             print_usage();
@@ -86,7 +95,7 @@ int main(int argc, char **argv) {
             struct in_addr addr;    // ping address
             if (inet_aton(optarg, &addr) == 0) {
                 fprintf(stderr, "malformed ping address: %s\n", optarg);
-                exit(1);
+                exit(EXIT_FAILURE);
             }
             args.addr.s_addr = addr.s_addr;
             break;
@@ -96,11 +105,23 @@ int main(int argc, char **argv) {
             break;
         case 'o':
             args.option = atoi(optarg);
-            if (args.option != 0 && args.option != 1 && args.option != 136 && args.option != 148) {
-                fprintf(stderr, "not supported IP option: %s\n", optarg);
-                exit(1);
+            if (args.option != EOOL && args.option != NOP && args.option != SID && args.option != RTRALT) {
+                fprintf(stderr, "IP option not supported: %s\n", optarg);
+                exit(EXIT_FAILURE);
             }
             break;
+        case 'l': {
+            // When the IP_HDRINCL option is set, datagrams will not be
+            // fragmented and are limited to the interface MTU.
+            args.data_len = atoi(optarg);
+            // mtu - max ip header - icmp header
+            int max_len = 1500 - 60 - 8;
+            if (args.data_len > max_len) {
+                fprintf(stderr, "exceeded max ping data length: %d\n", max_len);
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
         default:
             print_usage();
             exit(EXIT_FAILURE);
@@ -126,14 +147,15 @@ int main(int argc, char **argv) {
     }
 
     int fd;
-    if ((fd = socket(AF_INET, SOCK_RAW, IPPROTO_IGMP)) < 0) {
-        fprintf(stderr, "socket() failed: %s\n", strerror(errno));
+    // IPPROTO_RAW implies enabled IP_HDRINCL
+    if ((fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
+        fprintf(stderr, "socket(SOCK_RAW) failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     int val = 1;
     if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &val, sizeof(val)) < 0) {
-        fprintf(stderr, "setsockopt() failed: %s\n", strerror(errno));
+        fprintf(stderr, "setsockopt(IP_HDRINCL) failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
@@ -166,18 +188,38 @@ int main(int argc, char **argv) {
     int ip_hlen = 20;
     if (args.option != (uint8_t)-1) {
         // EOOL, NOP, SID, RTRALT
-        ip_hlen += 4;
+        if (args.option == EOOL || args.option == NOP) {
+            ip_hlen += 1;
+        } else if (args.option == SID || args.option == RTRALT) {
+            ip_hlen += 4;
+        }
     }
 
+    ip_hlen = (ip_hlen + 3) & (~3);
+
     // append icmp echo w/o data
-    uint16_t ip_len = ip_hlen + 8;
+    uint16_t ip_len = ip_hlen + 8 + args.data_len;
 
     char *pkt = malloc(ip_len);
     memset(pkt, 0, ip_len);
 
-    do {
-        struct ip *iphdr = (struct ip *)pkt;
+    struct ip *iphdr = (struct ip *)pkt;
+    struct icmphdr *icmphdr = (struct icmphdr *)((char *)iphdr + ip_hlen);
+    char *data = (char *)icmphdr + 8;
 
+    int iter = 0;
+    for (;;) {
+        for (int i = '0'; i <= 'z' && args.data_len; i++, args.data_len--) {
+            data[i - '0' + iter * ('z' - '0' + 1)] = (char)i;
+        }
+
+        if (!args.data_len) {
+            break;
+        }
+        iter++;
+    }
+
+    do {
         // IP v4
         iphdr->ip_v = 4;
         iphdr->ip_hl = ip_hlen / 4;
@@ -185,30 +227,29 @@ int main(int argc, char **argv) {
         iphdr->ip_len = htonl(ip_len);
         iphdr->ip_id = htons(0);
         iphdr->ip_off = htons(IP_DF);
-        iphdr->ip_ttl = 1;
+        iphdr->ip_ttl = 64;
         iphdr->ip_p = IPPROTO_ICMP;
         iphdr->ip_src = ((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr;
         iphdr->ip_dst.s_addr = args.addr.s_addr;
         // ip option
         char *opt = (char *)(iphdr + 1);
-        if (args.option == 1) { // NOP
-            opt[0] = 1;
-        } else if (args.option == 136) { // SID
-            *(uint32_t *)opt = htonl(136 << 24 | 4 << 16 | 0);
-        } else if (args.option == 148) { // RTRALT
-            *(uint32_t *)opt = htonl(148 << 24 | 4 << 16 | 0);
+        if (args.option == NOP) { // NOP
+            opt[0] = NOP;
+        } else if (args.option == SID) { // SID
+            *(uint32_t *)opt = htonl(SID << 24 | 4 << 16 | 0);
+        } else if (args.option == RTRALT) { // RTRALT
+            *(uint32_t *)opt = htonl(RTRALT << 24 | 4 << 16 | 0);
         }
-        iphdr->ip_sum = htons(checksum((uint16_t *)&iphdr, 20));
+        // checksum the ip header
+        iphdr->ip_sum = checksum((uint16_t *)&iphdr, ip_hlen);
 
-        struct icmphdr *icmp = (struct icmphdr *)((char *)iphdr + ip_hlen);
-        icmp->type = ICMP_ECHO;
-        icmp->code = 0;
-        icmp->checksum = 0;
-        icmp->un.echo.id = 0;
-        icmp->un.echo.sequence = seq++;
-
+        icmphdr->type = ICMP_ECHO;
+        icmphdr->code = 0;
+        icmphdr->checksum = 0;
+        icmphdr->un.echo.id = id;
+        icmphdr->un.echo.sequence = seq++;
         // checksum the icmp header & data
-        icmp->checksum = htons(checksum((uint16_t *)icmp, 8));
+        icmphdr->checksum = checksum((uint16_t *)icmphdr, ip_len - ip_hlen);
 
         struct sockaddr_in dst;
         memset(&dst, 0, sizeof(dst));
@@ -223,6 +264,7 @@ int main(int argc, char **argv) {
         sleep(args.interval);
     } while (!stopped);
 
+    free(pkt);
     close(fd);
     free(args.if_name);
 
